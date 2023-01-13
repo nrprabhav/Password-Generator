b13b8d3549bfcb8cc8e8915af26d6c4343341f53
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-01-13 19:41:39 +0000

    Check if alleast one kind of character is allowed. Alert and take inputs again if none are allowed

--- javascript/script.js
+++ javascript/script.js
@@ -97,21 +97,25 @@ function getPasswordOptions() {
     //Check if password is between 10 and 64
     if ((parseInt(passwordLength) < 10) | (parseInt(passwordLength) > 64)) {
       alert("Choose a number between 10 and 64");
-    }else{
+    } else {
       break;
     }
   }
-  allowUpper = confirm("Do you want UPPER CASE CHARACTERS in the password?");
-  allowLower = confirm("Do you want LOWER CASE CHARACTERS in the password?");
-  allowNumber = confirm("Do you want NUMBERS in the password?");
-  allowSpecial = confirm("Do you want SPECIAL CHARACTERS in the password?");
-
-  return [passwordLength, allowSpecial, allowNumber, allowLower, allowUpper];
+  while (true) {
+    allowUpper = confirm("Do you want UPPER CASE CHARACTERS in the password?\n(Select OK to allow and Cancel to not allow)");
+    allowLower = confirm("Do you want LOWER CASE CHARACTERS in the password?\n(Select OK to allow and Cancel to not allow)");
+    allowNumber = confirm("Do you want NUMBERS in the password?\n(Select OK to allow and Cancel to not allow)");
+    allowSpecial = confirm("Do you want SPECIAL CHARACTERS in the password?\n(Select OK to allow and Cancel to not allow)");
+    if (allowUpper || allowLower || allowNumber || allowSpecial)
+      return [passwordLength, allowSpecial, allowNumber, allowLower, allowUpper];
+    else
+      alert("You need to allow atleast one type of character");
+  }
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  let randomIndex = Math.floor(Math.random()*(arr.length));
+  let randomIndex = Math.floor(Math.random() * (arr.length));
   //alert(arr[randomIndex]);
   return arr[randomIndex];
 }
@@ -121,17 +125,17 @@ function generatePassword() {
   let passwordOptions = getPasswordOptions();
   let passwordLength = passwordOptions[0];
   let randomPassword = "";
-  for(i=0;i<passwordLength;i++){
+  for (i = 0; i < passwordLength; i++) {
     // Choose which type of character to use
     let characterType = 0;
-    while(true){
-      characterType = Math.floor(Math.random()*4);
-      if(passwordOptions[characterType+1]){
+    while (true) {
+      characterType = Math.floor(Math.random() * 4);
+      if (passwordOptions[characterType + 1]) {
         break;
       }
     }
     // Choose the character in the particular type
-    switch(characterType){
+    switch (characterType) {
       case 0:
         randomPassword += getRandom(specialCharacters);
         break;
